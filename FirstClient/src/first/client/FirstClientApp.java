0825661044ed4caaf0c7c177f62c77bf777d14bc
jameswang6 <jameswang6>
2008-05-27 11:57:11 +0000

    Init File

---
+++ FirstClient/src/first/client/FirstClientApp.java
@@ -0,0 +1,14 @@
+package first.client;
+
+public class FirstClientApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Say Thanks to the CVS host provider");
+	}
+
+}
